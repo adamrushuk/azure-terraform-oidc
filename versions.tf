a01e864371df0dadf748a5e7b3015962cbb8875f
Adam Rush <adamrushuk@gmail.com>
2022-10-23 19:33:08 +0100

    Enabled azuread_auth for terraform

--- versions.tf
+++ versions.tf
@@ -1,13 +1,14 @@
 terraform {
   required_version = ">= 1.0"
 
-  # terraform remote state
+  # https://developer.hashicorp.com/terraform/language/settings/backends/azurerm#example-configuration
   backend "azurerm" {
-    # resource_group_name  = "arsh-rg-tfstate"
-    # storage_account_name = "arshsttfstateeastus"
-    container_name       = "terraform"
-    key                  = "terraform.tfstate"
-    use_oidc             = true
+    container_name = "terraform"
+    key            = "terraform.tfstate"
+    use_oidc       = true
+
+    # SP requires "Storage Blob Data Contributor" on the container
+    use_azuread_auth = true
   }
 
   required_providers {
@@ -29,15 +30,9 @@ provider "azurerm" {
   # https://registry.terraform.io/providers/hashicorp/azurerm/latest/docs/guides/service_principal_oidc#configuring-the-service-principal-in-terraform
   use_oidc = true
 
-  # TODO
   # SP requires "Storage Blob Data Contributor" on the container
-  # use_azuread_auth = true
+  use_azuread_auth = true
 
   # https://registry.terraform.io/providers/hashicorp/azurerm/latest/docs/guides/features-block
-  features {
-    resource_group {
-      # required to cleanup velero snapshot(s) from resource group
-      prevent_deletion_if_contains_resources = false
-    }
-  }
+  features {}
 }
